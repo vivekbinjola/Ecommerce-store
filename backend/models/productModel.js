bcbd4fc2f935c6123ac543f5a6e5ccc58021187b
vivek binjola <vivekbinjola23@gmail.com>
2021-07-14 14:31:04 +0530

    added image in database

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,85 +1,80 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const reviewSchema = mongoose.Schema({
-    name:{ type:String, required:true},
-    rating:{ type:Number, required:true,},
-    comment:{ type:String, required:true}
-
-},{
-    timestamps: true
-})
-
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const productSchema = mongoose.Schema(
-    {
-
-    user:{
-            type: mongoose.Schema.Types.ObjectId,
-            required:true,
-            ref:'User'
-        },
-
-    name:{
-        type:String,
-        required: true
-        
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
     },
 
-    brand:{
-        type: String,
-        required: true,
-        
+    name: {
+      type: String,
+      required: true,
     },
 
-    category:{
-        type:String,
-        required: true
-        
+    brand: {
+      type: String,
+      required: true,
     },
 
-    price:{
-        type:Number,
-        required: true,
-        default: 0
-        
+    category: {
+      type: String,
+      required: true,
     },
 
-    rating:{
-        type:Number,
-        required: true,
-        default: 0
-        
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
     },
 
-    reviews:[reviewSchema],
-
-    numReviews:{
-        type:Number,
-        required: true,
-        default:0
-        
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
     },
 
-    description:{
-        type:String,
-        required: true
-        
+    reviews: [reviewSchema],
+
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
     },
 
-    countInStock:{
-        type:Number,
-        required: true,
-        default:0
-        
-    }
- 
-  
-},{
-    timestamps:true
-})
+    description: {
+      type: String,
+      required: true,
+    },
 
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
 
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Product = mongoose.model('Product',productSchema)
+const Product = mongoose.model("Product", productSchema);
 
-export default Product
+export default Product;
--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,30 @@
-import express from 'express';
-import dotenv from'dotenv'
-import  colors  from "colors";
-import connectDB from './config/db.js'
-import productRoutes from './routes/productRoutes.js'
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app= express()
+const app = express();
 
-app.get('/', (req,res)=>{
-    res.send('API is running....')
-})
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
 
-app.use('/api/products', productRoutes )
+app.use("/api/products", productRoutes);
 
-const PORT = process.env.PORT || 5000
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(
-    PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
-    )
-
-
-
-
-
-
-
-
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
